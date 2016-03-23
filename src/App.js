28bc3c11e10d59263e77569c021f4775ea25cf86
Zill Ding <zilldzy@gmail.com>
2016-03-23 15:58:18 +0800

    Add social sites profiles

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ export class App extends Component {
 
 class Content extends Component {
   render() {
+    const linkStyle = {
+      marginLeft: 5
+    }
+
     return (
       <div>
         <img
@@ -21,6 +25,36 @@ class Content extends Component {
           src="http://www.gravatar.com/avatar/9af45a0d5e1c3330dfb195733953b63c?s=150" />
         <h5>Zill Ding</h5>
         <p>Full Stack Developer</p>
+        <p>
+          <i className="fa fa-github fa-lg"></i>
+          <a href="https://github.com/zillding" style={linkStyle}>
+            zillding
+          </a>
+        </p>
+        <p>
+          <i className="fa fa-twitter fa-lg"></i>
+          <a href="https://twitter.com/ZillDing" style={linkStyle}>
+            @ZillDing
+          </a>
+        </p>
+        <p>
+          <i className="fa fa-facebook-official fa-lg"></i>
+          <a href="https://www.facebook.com/zeyu.ding.1" style={linkStyle}>
+            Zeyu Ding
+          </a>
+        </p>
+        <p>
+          <i className="fa fa-google-plus"></i>
+          <a href="https://plus.google.com/u/0/+ZeyuDing" style={linkStyle}>
+            Zeyu Ding
+          </a>
+        </p>
+        <p>
+          <i className="fa fa-medium"></i>
+          <a href="https://medium.com/@ZillDing" style={linkStyle}>
+            Zill Ding
+          </a>
+        </p>
       </div>
     )
   }
