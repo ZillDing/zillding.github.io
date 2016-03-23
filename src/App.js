f87f866be5b40ec7c59cf4f48cd8c02556eaba1a
Zill Ding <zilldzy@gmail.com>
2016-03-23 15:33:39 +0800

    Create a front page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import React, { Component } from 'react'
-import { NICE, SUPER_NICE } from './colors'
-
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { counter: 0 }
-    this.interval = setInterval(() => this.tick(), 1000)
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    })
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    )
-  }
-}
 
 export class App extends Component {
   render() {
     return (
+      <div className="valign-wrapper" style={{height: '100%'}}>
+        <div className="valign center" style={{width: '100%'}}>
+          <Content />
+        </div>
+      </div>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
       <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
+        <img
+          className="circle responsive-img"
+          src="http://www.gravatar.com/avatar/9af45a0d5e1c3330dfb195733953b63c?s=150" />
+        <h5>Zill Ding</h5>
+        <p>Full Stack Developer</p>
       </div>
     )
   }
